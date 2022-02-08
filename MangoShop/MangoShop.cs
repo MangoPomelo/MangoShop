b97d5a1e46ae792bb56fc6d95960ae963d399bae
MangoPomelo <3232426550@qq.com>
2022-02-08 14:46:31 +0800

    fix: propmt on purchase failed

--- MangoShop/MangoShop.cs
+++ MangoShop/MangoShop.cs
@@ -19,7 +19,7 @@ namespace MangoShop
             { "ProductNotFound", "Product not found!" },
             // BuyCommand
             { "PurchaseSucceed", "You received {0}!" },
-            { "PurchaseFailed", "Your xp is less than {0} or the product is banned!" },
+            { "PurchaseFailed", "You don't have enough xp or the product is banned!" },
             // SellCommand
             { "SellingSucceed", "You received {0} xp!" },
             { "SellingFailed", "You don't have enough items or the product cannot be sold!" },
